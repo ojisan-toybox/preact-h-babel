55b431bf36221796210bc9183bd85f4efed944de
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-01 05:53:03 +0900

    update

--- dist.js
+++ dist.js
@@ -1,23 +1,71 @@
-import { h, Fragment } from "preact";
-import { useState } from "preact/compat";
+import { h, Fragment, Component } from "preact";
 
-const TestComponent = () => {
-  const [state, setState] = useState(0);
-  return h(
-    Fragment,
-    null,
-    h(
-      "button",
-      {
-        onClick: () => {
-          setState(state + 1);
-        },
-      },
-      "add"
-    ),
-    h("div", null, h("span", null, "count: "), h("span", null, state)),
-    "3",
-    false,
-    "3"
-  );
-};
+class App extends Component {
+  constructor() {
+    this.state = {
+      count: 10000000,
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ ...this.state,
+      count: 0,
+      data: [{
+        name: "taro"
+      }, {
+        name: "hanako"
+      }]
+    });
+  }
+
+  componentWillReceiveProps(next) {
+    console.log("next.props:", next.props);
+  }
+
+  render() {
+    return h("div", null, h("section", null, h("h1", null, "counting area"), h("span", null, "count: "), h("span", null, this.state.count), h("button", {
+      onClick: () => this.setState({ ...this.state,
+        count: this.state.count + 1
+      })
+    }, "add")), h("section", null, h("h1", null, "user data area"), h("ul", null, this.state.data.map((d, i) => h(ListItem, {
+      name: d.name,
+      handleDelete: () => {
+        this.setState({ ...this.state,
+          data: this.state.data.filter((_, j) => {
+            return i !== j;
+          })
+        });
+      }
+    }))), h("form", {
+      onSubmit: e => {
+        e.preventDefault();
+        const userName = e.target["name"].value;
+        this.setState({ ...this.state,
+          data: [...this.state.data, {
+            name: userName
+          }]
+        });
+      }
+    }, h("input", {
+      name: "name"
+    }), h("button", {
+      type: "submit"
+    }, "add"))));
+  }
+
+}
+
+class ListItem extends Component {
+  componentWillReceiveProps(nextProps, prevProps) {
+    console.log("next.props:", nextProps);
+    console.log("next.props:", prevProps);
+  }
+
+  render() {
+    return h(Fragment, null, h("li", null, this.props.name), h("button", {
+      onClick: () => this.props.handleDelete()
+    }, "delete"));
+  }
+
+}
--- index.js
+++ index.js
@@ -1,24 +1,87 @@
-import { h, Fragment } from "preact";
-import { useState } from "preact/compat";
+import { h, Fragment, Component } from "preact";
 
-const TestComponent = () => {
-  const [state, setState] = useState(0);
+class App extends Component {
+  constructor() {
+    this.state = {
+      count: 10000000,
+      data: [],
+    };
+  }
 
-  return (
-    <>
-      <button
-        onClick={() => {
-          setState(state + 1);
-        }}
-      >
-        add
-      </button>
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      count: 0,
+      data: [{ name: "taro" }, { name: "hanako" }],
+    });
+  }
+
+  componentWillReceiveProps(next) {
+    console.log("next.props:", next.props);
+  }
+
+  render() {
+    return (
       <div>
-        <span>count: </span>
-        <span>{state}</span>
+        <section>
+          <h1>counting area</h1>
+          <span>count: </span>
+          <span>{this.state.count}</span>
+          <button
+            onClick={() =>
+              this.setState({ ...this.state, count: this.state.count + 1 })
+            }
+          >
+            add
+          </button>
+        </section>
+        <section>
+          <h1>user data area</h1>
+          <ul>
+            {this.state.data.map((d, i) => (
+              <ListItem
+                name={d.name}
+                handleDelete={() => {
+                  this.setState({
+                    ...this.state,
+                    data: this.state.data.filter((_, j) => {
+                      return i !== j;
+                    }),
+                  });
+                }}
+              ></ListItem>
+            ))}
+          </ul>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const userName = e.target["name"].value;
+              this.setState({
+                ...this.state,
+                data: [...this.state.data, { name: userName }],
+              });
+            }}
+          >
+            <input name="name"></input>
+            <button type="submit">add</button>
+          </form>
+        </section>
       </div>
-      3{false}
-      {"3"}
-    </>
-  );
-};
+    );
+  }
+}
+
+class ListItem extends Component {
+  componentWillReceiveProps(nextProps, prevProps) {
+    console.log("next.props:", nextProps);
+    console.log("next.props:", prevProps);
+  }
+  render() {
+    return (
+      <>
+        <li>{this.props.name}</li>
+        <button onClick={() => this.props.handleDelete()}>delete</button>
+      </>
+    );
+  }
+}
